26d801123207dd26688d61de2f5ea984f5aa3a49
Hendrik Prinsloo <hendrik.prinsloo@cleva.com>
2018-09-12 16:44:20 +0200

    Updated to prevent security vulnerabilities

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  */
 var googleTTS = require('google-tts-api');
 var player = require('play-sound')({});
-var wget = require('node-wget');
+var wget = require('wget-improved');
 
 /**
  * Accepts a string as parameter, converts it into an mp3 and plays the sound
@@ -14,20 +14,18 @@ var wget = require('node-wget');
  */
 function basicTtsPlayer(string) {
     googleTTS(string, 'en', 1).then(function (url) {
-        wget({
-            url: url,
-            dest: '/tmp/basic-tts-player.mp3'
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error: Failed to generate audio file');
-            } else {
-                player.play('/tmp/basic-tts-player.mp3', function(err){
-                    if (err) throw err
-                    return {
-                        url: url
-                    };
-                });
-            }
+        const options = { };
+        let download = wget.download(url, '/tmp/basic-tts-player.mp3', options);
+        download.on('error', function(err) {
+            console.log(err);
+        });
+        download.on('end', function(output) {
+            player.play('/tmp/basic-tts-player.mp3', function(err){
+                if (err) throw err
+                return {
+                    url: url
+                };
+            });
         });
     })
     .catch(function (err) {
